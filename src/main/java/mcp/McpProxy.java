f675855b80e438fd4eb77a660f40aa46d4eddfc1
Nate Mara <natemara@gmail.com>
2015-04-09 19:10:20 -0400

    add mcp proxy file

---
+++ src/main/java/mcp/McpProxy.java
@@ -0,0 +1,10 @@
+import com.etsy.net.JUDS;
+import com.etsy.net.UnixDomainSocketClient;
+
+public class McpProxy {
+	UnixDomainSocketClient client;
+
+	public UnixDomainSocketClient(String fd) {
+
+	}
+}
