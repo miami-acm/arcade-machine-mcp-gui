208e7449e383f4b0a569daca2b8e35fe27bd405a
Nate Mara <natemara@gmail.com>
2015-04-10 00:27:13 -0400

    write constructor for game to take string from mcp

--- src/main/java/mcp/Game.java
+++ src/main/java/mcp/Game.java
@@ -1,12 +1,16 @@
 package mcp;
 
 import processing.core.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Game {
 	String name;
 	PImage image;
 	String path;
 	PApplet parent;
+	static final String GAME_RULE = "'([-_a-zA-Z0-9. ]+)' '([-_a-zA-Z0-9. /]+)' '([-_a-zA-Z0-9. /]+)'";
+	static final Pattern GAME_PATTERN = Pattern.compile(GAME_RULE);
 
 	public Game(PApplet parent, String name, String path, String imagePath) {
 		this.parent = parent;
@@ -18,5 +22,27 @@ public class Game {
 	public void run() {
 		parent.println("Running game " + name);
 		parent.exit();
+	}
+
+	public static Game fromString(PApplet parent, String line) {
+		System.out.println("parsing line");
+		System.out.println(line);
+		Matcher lineMatcher = GAME_PATTERN.matcher(line);
+
+		if (!lineMatcher.matches()) {
+			throw new IllegalArgumentException("The inputted line was malformed.\n" + line);
+		}
+
+		String name = lineMatcher.group(1);
+		String path = lineMatcher.group(2);
+		String image = lineMatcher.group(3);
+
+		Game g = new Game(parent, name, path, image);
+		System.out.println(g);
+		return g;
+	}
+
+	public String toString() {
+		return String.format("Game [name=%s path=%s image=%s]", name, path, image);
 	}
 }
